0a84f75d0ebd625faf3aca05a3dddce45b285158
Jaime Alvarez <jaime.alvarez1984@hotmail.com>
2020-04-16 12:48:44 -0500

    - SQLite gem removed.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorites", force: :cascade do |t|
     t.string "photo_id"
     t.string "photo_url_small"
